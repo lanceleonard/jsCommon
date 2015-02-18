6539bb01e099123ad3028a0e0cad39543f2e9575
Lance Leonard <lance.leonard@outlook.com>
2015-02-17 20:11:28 -0800

    Tweaks, jsdoc-ification, and planning
    Setting the groundwork for the phase.  Added change.log

--- jsCommon.js
+++ jsCommon.js
@@ -1,7 +1,26 @@
-// This library contain common helper functions designed to simplify
-// the development of web applications.  
+/**
+ * This library contain common helper functions designed to simplify
+ * the development of web applications.  It's a work in progress.
+ *
+ * TODO:: 
+ * 0.  Consolidate disparate versions of the library into one canoncial version to, ahem, rule them all.  Get to v1.0 release.
+ * 1.  Convert inline documentation to jsdoc style and add tools for genetating the docs.
+ * 2.  Review the use of IDs and objects as parameters.  Consider unifying use into a single process or openly supported both techniques.  Use whatever choice consistently.
+ * 3.  When doc generation is working, incororporate the resulting docs into the git wiki.
+ * 4.  Rework to register default handlers for error checking, results reporting and similar activities.
+ * 5.  Add functions that allow shortcuts for common tasks, e.g. addLine();
+ * 6.  Add functionality to register common panel names by default, as well as the 
+ * ability to change the names as needed by individual apps.
+ */
+
+  // Constants
+  var kPathSeparator = "/";
 
   function clearPanelText( sTargetPanel ) {
+  /**
+   * Clears the text content of a panel.  An intermediary function, this is generally called by other functions.
+   * @param {string} panel ID - The ID assigned to the panel, assumed to be unique to the given page.
+   */
 
     // TODO:: Strengthen error checking here.
     var o = getElementObject( sTargetPanel );
@@ -14,32 +33,48 @@
   
 
   function clearResults() { 
-    // Clears the results panel.  Companion function to updateResults().
+  /**
+   * Clears the text content of the results panel.  Companion function to updateResults().
+   */
+
+    // TODO:: Rework to register pResults by default and add functions to allow the 
+    // resgistration of other pnael objects, perhaps even a push/pop mechanism.
     clearPanelText( "pResults" );
   }
 
   function clearStatus() { 
-  // Clears the status panel.  Companion function to updateStatus().
+  /**
+   * Clears the text content of the status panel.  Companion function to updateStatus().
+   */
     clearPanelText( "pStatus" );
   }
 
-  function extractFilePath( sFullFilename ) {
-// Given a URL, this returns the path without the filename.  (See also: 
-// reduceFilename)
+  function extractFilePath( sFullFilename, bIncludeTrailing ) {
+  /**
+   * Given a URL, this returns the path without the filename.  (See also: reduceFilename)
+   * @param {string} full filename - The fully qualified filename to parse
+   * @param {boolean} include trailing - (Optional) Include trailing separator (default: true)
+   * @returns {string} the file path portion of the full filename, if any.
+   */
 
+   var sTrailingSep = ( bIncludeTrailing == true) ? kPathSeparator : "";
    var sResult = "";
    if ( sFullFilename != "" ) {
-      var aTokens = sFullFilename.split( '/' );
+      var aTokens = sFullFilename.split( kPathSeparator );
       if ( aTokens.length > 0 ) {
          aTokens.pop();
-         sResult = aTokens.join( '/' ) + '/';
+         sResult = aTokens.join( kPathSeparator ) + kPathSeparator;
 	  }
    }
    return sResult;
 }
 
-function getElementObject( sObjectID ) {
-  // Gets a reference to an object in a webpage, with limited error-checking.
+  function getElementObject( sObjectID ) {
+  /**
+   * Returns the object associated with a given ID value.  Note that this presumes that IDs are unique.
+   * @param {string} ID value to search for.
+   * @returns {object} Object with the ID value.
+   */
   var d = document;
     var oRetval = d.getElementById( sObjectID );
     if ( oRetval == null ) {
@@ -52,11 +87,15 @@ function getElementObject( sObjectID ) {
 
   
   function getTextContent( oTarget ) {
-  // Returns the text inside an element object, with error-checking.
+  /**
+   * Returns the text inside an element object using a technique that supports older browsers.
+   * @param {object} target object - The object to process
+   * @returns {string} the text content associated with the object, if any.
+   */
    
    var sResult = null;
    if ( oTarget == null ) {
-      var sError = "GetTextContent could not set text; " + 
+      var sError = "GetTextContent could not get text; " + 
                    "object is not assigned.";
       handleError( sError );
     } else {
@@ -67,8 +106,14 @@ function getElementObject( sObjectID ) {
   }
 
   function handleError( sErrorMsg ) {
-  // Displays an error message using the technique appropriate for this 
-  // application.  Note that the preference is to send errors to the console.
+  /**
+   * Handles an error message using the currently registerd process,
+   * @param {string} error message - The message to be handled
+   */
+   
+    // TODO: 
+    // 1.  Rework to use the console as the default handler.
+    // 2.  Rework to default to the output panel, rather than the results panel.
     var s = "Development Error: " + sErrorMsg;
     var w = window;
     if ( w.console ) {
@@ -78,37 +123,48 @@ function getElementObject( sObjectID ) {
     }
   }
 
-function reduceFilename( sFullFilename ) {
-// Given a URL, this returns just the filename.  See also: extractFilePath.
-
+  function reduceFilename( sFullFilename ) {
+  /**
+   * Returns the filename associated with a fully qualified filename.  
+   * @param {string} full filename - The filename to process
+   * @returns {string} the filename portion of the full filename.
+   * @see {@link extractFilePath}
+   */
+   
    var sResult = "";
    if ( sFullFilename != "" ) {
-      var aTokens = sFullFilename.split( '/' );
+      var aTokens = sFullFilename.split( kPathSeparator );
       if ( aTokens.length > 0 ) {
          sResult = aTokens[ aTokens.length - 1 ];
 	  }
    }
    return sResult;
-}
+  }
 
   function registerEvent( sTargetID, sEventName, fnHandler ) 
-  // Registers an event handler.
-  // TODO: 1) Rework to search for the attribute before using
-  // attachEvent.  OnEventName is standard-based; attachEvent
-  // is not. 2) Add filter to exclude the DOMxxx events.  Not
-  // every event has an "on" version that needs fallback.
+  /**
+   * Registers an event handler using techniques designed to provide graceful fallback for older browsers.
+   * @param {string} ID of the object to receive the event handler.  Pass 'document' or 'window' to assigned handlers to the corresponding DOM objects.
+   * @param {string} Name of the event to handle (without 'on' prefix).
+   * @param {function} Handle to the function to handle the event.
+   */
+
+  // TODO: 
+  // 1) Rework to search for the attribute before using attachEvent.  OnEventName is standard-based; attachEvent is not. 
+  // 2) Add filter to exclude the DOMxxx events.  Not every event has an "on" version that needs fallback.
+  // 3) Currently, there's no fall back when the object cannot be found; need to provide something for that case, even if it's only a basic error handler.
       
   {
      var oTarget = null;
      // See if the target is a supported built-in object.
 
-	 if ( sTargetID == "document" ) {
-	   oTarget = document; 
-	 } else if ( sTargetID == "window" ) {
-	   oTarget = window;
-	 } else {
+	  if ( sTargetID == "document" ) {
+	    oTarget = document; 
+	  } else if ( sTargetID == "window" ) {
+	    oTarget = window;
+	  } else {
        oTarget = document.getElementById( sTargetID );
-     }
+    }
 	 
     if ( oTarget != null ) 
     {
@@ -126,7 +182,11 @@ function reduceFilename( sFullFilename ) {
   }
   
   function setTextContent( oTarget, sNewText ) {
-  // Set the text inside an element object, with error-checking.
+  /**
+   * Set the text inside an element object, with error-checking.
+   * @param {object} The object to update.
+   * @param {string} Content to be put into the object.
+   */
    
    if ( oTarget == null ) {
       var sError = "setTextContent could not set text; object not assigned.";
@@ -139,7 +199,11 @@ function reduceFilename( sFullFilename ) {
   }
 
   function updatePanelText( sTargetPanel, sMessage )
-  // Writes text to a "panel" (div, p, span, etc.).
+  /**
+   * Writes text to a "panel" (div, p, span, etc.).
+   * @param {string} sTargetPanel - the ID of the object to be updated.
+   * @param {string} sMessage - Message to be placed in the target object.
+   */
   {
 
     // TODO: Strengthen error check and input validation. 
@@ -162,15 +226,24 @@ function reduceFilename( sFullFilename ) {
 
   
   function updateResults( sMessage )
-  // Wrapper function for displaying a line of text in the result panel
+  /**
+   * Writes text to the result panel.
+   * @param {string} sMessage - Message to be placed in the results object.
+   */
   {
+    // TODO: Revise to sent results to registered results panel.
     sResultPanelName = "pResults";
     updatePanelText( sResultPanelName, sMessage );
   }
 
   function updateStatus( sMessage, bTimestamp ) 
-  // Wrapper function for displaying a message to the user without using alert.
+  /**
+   * Writes text to the status panel.  Used to avoid using alert() and other deprecated APIs.
+   * @param {string} sMessage - Message to be placed in the status object.
+   * @param {boolean} (Optional) Timestamp flag - Write timestamp (false, by default)
+   */
   {
+    // TODO: Revise to sent results to registered results panel.
     sStatusPanelName = "pStatus";
     var bAddTime = ( bTimestamp != null ) ? true : false;
 
@@ -187,6 +260,8 @@ function reduceFilename( sFullFilename ) {
   }
 
   function sny() 
-  // Simple message for those using the program before it's ready for review.
+  /**
+   * Simple message to indicate that the feature is incomplete; generally used during development.
+   */
   { updateResults( "Sorry; this is not implemented yet." ); }
 
